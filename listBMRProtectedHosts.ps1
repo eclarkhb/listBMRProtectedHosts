645f5fb4557f375332eb59acc2aa668ed259bf70
eclarkhb <67005086+eclarkhb@users.noreply.github.com>
2023-07-17 13:48:08 -0700

    Add files via upload

---
+++ listBMRProtectedHosts.ps1
@@ -0,0 +1,39 @@
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,  # the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username,  # username (local or AD)
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password = $null,
+    [Parameter()][string]$scriptname = 'cobmrcfg' # cobmrcfg is the default pre-script name for linux hosts
+)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authenticate
+if($useApiKey){
+    apiauth -vip $vip -username $username -domain $domain -useApiKey -password $password
+}else{
+    apiauth -vip $vip -username $username -domain $domain -password $password
+}
+
+#find all of the kPhysical Protection Groups
+$foundBMR = false
+$jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true&environments=kPhysical"
+$paramPaths = @{'kFile' = 'fileProtectionTypeParams'; 'kVolume' = 'volumeProtectionTypeParams'}
+
+foreach($job in $jobs.protectionGroups){
+    $paramPath = $paramPaths[$job.physicalParams.protectionType]
+    if(($job.physicalParams.$paramPath.prePostScript -ieq $scriptname) -or ($job.physicalParams.$paramPath.prePostScript -ieq "$scriptname.bat")){
+        foreach($host in $job.physicalParams.$paramPath.objects){
+            $foundBMR = true
+            Write-Host $host.name
+        }
+    }
+}
+
+if($foundBMR -eq $false){
+        Write-Host "No BMR Protection found in any physical protection group" -ForegroundColor Yellow
+    }
